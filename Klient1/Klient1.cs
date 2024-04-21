f0c1d99d2b9528e69e9f0310f7c8921ed58d6f98
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-04-21 23:54:48 +0200

    Add files via upload

---
+++ Klient1/Klient1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KSR_WCF2;
+
+namespace Klient1
+{
+    public class Task1Client
+    {
+        public static void Main(string[] args)
+        {
+            var zadanie1 = new ServiceReference1.Zadanie1Client();
+
+            zadanie1.DlugieObliczeniaAsync();
+
+            for (int i = 0; i < 21; i++)
+            {
+                Console.WriteLine(zadanie1.Szybciej(i, 3 * i * i - 2 * i));
+            }
+
+            ((IDisposable)zadanie1).Dispose();
+        }
+    }
+}
